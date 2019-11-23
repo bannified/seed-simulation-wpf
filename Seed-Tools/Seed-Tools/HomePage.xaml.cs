d61b4b6667376807a97a0cb932df4c8677ccbc9c
bannified <freedomzelda_dc@hotmail.com>
2019-11-23 19:28:20 +0800

    Add HomePage

---
+++ Seed-Tools/Seed-Tools/HomePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Seed_Tools
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Simulation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeckButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RulesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
