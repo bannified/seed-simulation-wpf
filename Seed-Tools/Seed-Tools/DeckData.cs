19d12879ef08b84845f9908bffe8853330b0055f
bannified <freedomzelda_dc@hotmail.com>
2019-11-28 22:19:35 +0800

    Add simple save/load for decks

---
+++ Seed-Tools/Seed-Tools/DeckData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seed_Tools
+{
+    public class DeckData
+    {
+        public string DisplayName { get; set; }
+        public List<CardData> Cards { get; set; }
+    }
+}
--- Seed-Tools/Seed-Tools/DeckEditPage.xaml.cs
+++ Seed-Tools/Seed-Tools/DeckEditPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Json.Net;
 
 namespace Seed_Tools
 {
@@ -20,9 +21,15 @@ namespace Seed_Tools
     /// </summary>
     public partial class DeckEditPage : Page
     {
+        public DeckData ActiveDeckData { get; set; }
+
+        public System.Action<DeckData> OnDeckLoaded;
+
         public DeckEditPage()
         {
             InitializeComponent();
+            ActiveDeckData = new DeckData();
+            OnDeckLoaded += RefreshDeckDisplay;
         }
 
         private void OnRemoveCardClicked(object sender, float e)
@@ -33,6 +40,58 @@ namespace Seed_Tools
         private void OnAddCardClicked(object sender, float e)
         {
 
+        }
+
+        private void DeckLoadClicked(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.InitialDirectory = App.GetRootPath();
+            dialog.DefaultExt = ".dek";
+            dialog.Multiselect = false;
+            dialog.Filter = "Seed Deck Files (*.dek)|*.dek";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                if (System.IO.File.Exists(filename))
+                {
+                    string loadedText = System.IO.File.ReadAllText(filename);
+                    DeckData deck = JsonNet.Deserialize<DeckData>(loadedText);
+                    ActiveDeckData = deck;
+                    OnDeckLoaded?.Invoke(deck);
+                }
+            }
+        }
+
+        private void DeckSaveAsClicked(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.InitialDirectory = App.GetRootPath();
+            dialog.DefaultExt = ".dek";
+            dialog.Filter = "Seed Deck Files (*.dek)|*.dek";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                System.IO.FileStream fs = System.IO.File.Create(filename);
+                fs.Close();
+                string resultJson = JsonNet.Serialize(ActiveDeckData);
+                System.IO.File.WriteAllText(filename, resultJson);
+            }
+        }
+
+        private void DeckNameLostFocus(object sender, RoutedEventArgs e)
+        {
+            ActiveDeckData.DisplayName = DeckNameTextBox.Text;
+        }
+
+        private void RefreshDeckDisplay(DeckData data)
+        {
+            DeckNameTextBox.Text = data.DisplayName;
         }
     }
 }
