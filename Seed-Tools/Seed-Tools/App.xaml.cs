dee93134645ecd2d12c569b9116bfb42f59e9ef8
bannified <freedomzelda_dc@hotmail.com>
2019-11-28 23:22:29 +0800

    Add auto-save/load on app exit/start

--- Seed-Tools/Seed-Tools/App.xaml.cs
+++ Seed-Tools/Seed-Tools/App.xaml.cs
@@ -15,6 +15,20 @@ namespace Seed_Tools
 	/// </summary>
 	public partial class App : Application
 	{
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // load user settings
+            Seed_Tools.Properties.Settings.Default.Reload();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            // Save properties
+            Seed_Tools.Properties.Settings.Default.Save();
+        }
+
         public static string GetRootPath()
         {
             return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
