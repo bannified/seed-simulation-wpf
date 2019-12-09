26e7899fb5c22ca1195c555dbb58e7f471458faa
bannified <freedomzelda_dc@hotmail.com>
2019-12-09 23:11:19 +0800

    Add Suit class

---
+++ Seed-Tools/Seed-Tools/Suit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seed_Tools
+{
+    public class Suit
+    {
+        public string Name { get; set; } = "New Suit";
+
+        // Value used to comparing suits (set all to 0 if they're all equal)
+        public int Value { get; set; } = 0;
+
+        // Main image associated with this suit
+        public string ImagePath { get; set; } 
+    }
+}
